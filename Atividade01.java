5fed223c03d5a5a99d033b56f4da9191cf930603
Yago Rigon <37890602+YagoRigon@users.noreply.github.com>
2021-08-12 21:11:34 -0300

    Add files via upload

---
+++ Atividade01.java
@@ -0,0 +1,15 @@
+public class Atividade01 {
+    public static void main(String[] args) {
+        int i=1, resultado = 0, x=10;
+
+        System.out.println("Tabuada de " + x);
+        while(i <=10 )
+        {
+            resultado = 0;
+            resultado = x * i;
+            System.out.print("10 x " +i + " " + "=");
+            System.out.println(" "+resultado);
+            i++;
+        }
+    }
+}
